3ddb6f0823778cdb63b8014210919d5cdecb14b0
Abhay3011 <83626456+Abhay3011@users.noreply.github.com>
2023-03-19 19:42:23 +0530

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -132,18 +132,6 @@ async def info(file, event):
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
     )
-    stdout, stderr = process.communicate()
-    out = stdout.decode()
-    client = TelegraphPoster(use_api=True)
-    client.create_api_token("Mediainfo")
-    page = client.post(
-        title="Mediainfo",
-        author=((await event.client.get_me()).first_name),
-        author_url=f"https://t.me/{((await event.client.get_me()).username)}",
-        text=out,
-    )
-    return page["url"]
-
 
 def code(data):
     OK.update({len(OK): data})
